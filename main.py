bc125a761b2a01da3d43c99d973d0e5c570c0740
eddy-qiu <eqiu22@gmail.com>
2023-11-03 01:13:19 -0400

    mvp

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import openai
 import gradio
 import config
 
-messages = [{"role": "system", "content": "You are a psychologist and friend of a University student who is seeking mental health support."}]
+openai.api_key = config.OPENAI_KEY
+messages = [{"role": "system", "content": "You are a therapist and friend to a University student"}]
 
 def CustomChatGPT(user_input):
-        messages.append({"role": "user", "content": user_input})
-        response = openai.ChatCompletion.create(
-                model = "gpt-3.5-turbo",
-                messages = messages
-        )
-        ChatGPT_reply = response["choices"][0]["message"]["content"]
-        messages.append({"role": "assistant", "content": ChatGPT_reply})
-        return ChatGPT_reply
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
 
-app = gradio.Interface(fn=CustomChatGPT, inputs = "Input", outputs = "reponse", title = "TerrapinTalk", description = "A chatbot that provides mental health support to University of Maryland students.")
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "TerrapinTalk")
 
-app.launch()
+demo.launch(share=True)
